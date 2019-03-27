322cfed7195f3e6900f513a920697c7a4471e902
admin <admin@LAPTOP-PVLBOJ5E>
2019-03-27 16:19:15 +0800

    完成简单的微信接入程序开发

--- src/main/java/org/fkjava/weixin/weixinApplication.java
+++ src/main/java/org/fkjava/weixin/weixinApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 微信项目Application {
+public class weixinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(微信项目Application.class, args);
+		SpringApplication.run(weixinApplication.class, args);
 	}
 
 }
