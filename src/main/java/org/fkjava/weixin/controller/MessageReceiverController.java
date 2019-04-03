7b95955f169bec14d0103d48fc368002d26d7ee2
admin <admin@LAPTOP-PVLBOJ5E>
2019-04-03 21:08:50 +0800

    接受微信公众号平台发给我们的消息

--- src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
@@ -3,6 +3,7 @@ package org.fkjava.weixin.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,7 @@ public class MessageReceiverController {
         return echostr;
     }
     
+    @PostMapping
     public String onMessage(
     		@RequestParam("signature")String signature,
     		@RequestParam("timestamp")String timestamp,
